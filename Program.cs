54d32b1d48400c956d3dfe190ca0d8de2a28bdc6
RobCam <roberto.camacho4@gmail.com>
2024-12-11 16:40:26 +0100

    Added CRUD methods for exhibtion and owner in Program.cs

--- Program.cs
+++ Program.cs
@@ -2,17 +2,26 @@
 
 namespace MyCatExhibition;
 
-class Cat 
-{
-    public int CatId { get; set; }
-    public string Name { get; set; }
-}
 
 class Owner
 {
     public int OwnerId { get; set; }
     public string Name { get; set; }
 }
+
+class Exhibition
+{
+    public int ExhibitionId { get; set; }
+    public string Name { get; set; }
+    public DateTime ExhibitionDate { get; set; }
+}
+
+class Cat
+{
+    public int CatId { get; set; }
+    public string Name { get; set; }
+}
+
 
 class Program
 {
@@ -22,10 +31,27 @@ class Program
     {
         while (true)
         {
-            Console.WriteLine("1. Print ownerss");
+            Console.WriteLine("1. Print owners");
             Console.WriteLine("2. Add owner");
             Console.WriteLine("3. Update owner");
-            Console.WriteLine("4. Exit");
+            Console.WriteLine("4. Delete owner");
+
+            Console.WriteLine("----------------");
+            Console.WriteLine("5. Print exhibitions");
+            Console.WriteLine("6. Add exhibition");
+            Console.WriteLine("7. Update exhibition");
+            Console.WriteLine("8. Delete exhibition");
+
+            Console.WriteLine("----------------");
+            Console.WriteLine("9. Print cats");
+            Console.WriteLine("10. Add cat");
+            Console.WriteLine("11. Update cat");
+
+            Console.WriteLine("12. Delete cat");
+
+            Console.WriteLine("----------------");
+            Console.WriteLine("e. Exit");
+
             Console.Write("Select: ");
             string choice = Console.ReadLine();
             switch (choice)
@@ -40,6 +66,36 @@ class Program
                     UpdateOwner();
                     break;
                 case "4":
+                    DeleteOwner();
+                    break;
+
+                case "5":
+                    PrintExhibitions();
+                    break;
+                case "6":
+                    InsertExhibition();
+                    break;
+                case "7":
+                    UpdateExhibition();
+                    break;
+                case "8":
+                    DeleteExhibition();
+                    break;
+
+                case "9":
+       //             PrintCat();
+                    break;
+                case "10":
+         //           InsertCat();
+                    break;
+                case "11":
+          //          UpdateCat();
+                    break;
+                case "12":
+            //        DeleteCat();
+                    break;
+
+                case "e":
                     return;
                 default:
                     Console.WriteLine("Invalid choice");
@@ -47,6 +103,8 @@ class Program
             }
         }
     }
+
+    //---------------- Owner ----------------------------
 
     static void PrintOwners()
     {
@@ -78,4 +136,63 @@ class Program
 
         repo.UpdateOwner(id, name);
     }
+
+    static void DeleteOwner()
+    {
+        Console.Write("Ange id på den ägare du vill ta bort: ");
+        int id = Chelp.ReadInt();
+
+        repo.DeleteOwner(id);
+    }
+
+
+
+    //---------------- Exhibition ----------------------------
+
+    static void PrintExhibitions()
+    {
+        IEnumerable<Exhibition> exhibitions = repo.GetExhibitions();
+
+        foreach (Exhibition s in exhibitions)
+        {
+            Console.WriteLine($"Id: {s.ExhibitionId} Name: {s.Name} Date: {s.ExhibitionDate}");
+        }
+    }
+
+    static void InsertExhibition()
+    {
+        // Läsa in namn för ägare
+        Console.Write("Name: ");
+        string name = Chelp.ReadString();
+
+        Console.Write("Date of exhibition: ");
+        DateTime exhibitionDate = Chelp.ReadDate();
+
+
+        repo.InsertExhibition(name, exhibitionDate);
+    }
+
+    static void UpdateExhibition()
+    {
+        Console.Write("Ange id på utställning du vill uppdatera: ");
+        int id = Chelp.ReadInt();
+
+        // Uppdatera ägarens namn
+        Console.Write("Name: ");
+        string name = Chelp.ReadString();
+
+        Console.Write("Date of exhibition: ");
+        DateTime exhibitionDate = Chelp.ReadDate();
+
+        repo.UpdateExhibition(id, name, exhibitionDate);
+    }
+
+    static void DeleteExhibition()
+    {
+        Console.Write("Ange id på den utställning du vill ta bort: ");
+        int id = Chelp.ReadInt();
+
+        repo.DeleteExhibition(id);
+    }
+
 }
