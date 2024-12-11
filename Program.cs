19df5cb757f5e17c00b039a21bf97b2c8a84cf72
RobCam <roberto.camacho4@gmail.com>
2024-12-11 14:55:26 +0100

    Updated Program.cs and added a menu and methods PrintOwners, InstertOwner and UpdateOwner

--- Program.cs
+++ Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyCatExhibition;
+
+class Cat 
+{
+    public int CatId { get; set; }
+    public string Name { get; set; }
+}
+
+class Owner
+{
+    public int OwnerId { get; set; }
+    public string Name { get; set; }
+}
+
+class Program
+{
+    static DatabaseRepository repo = new DatabaseRepository();
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("1. Print ownerss");
+            Console.WriteLine("2. Add owner");
+            Console.WriteLine("3. Update owner");
+            Console.WriteLine("4. Exit");
+            Console.Write("Select: ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    PrintOwners();
+                    break;
+                case "2":
+                    InsertOwner();
+                    break;
+                case "3":
+                    UpdateOwner();
+                    break;
+                case "4":
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+    }
+
+    static void PrintOwners()
+    {
+        IEnumerable<Owner> owners = repo.GetOwners();
+        
+        foreach (Owner s in owners)
+        {
+            Console.WriteLine($"Id: {s.OwnerId} Name: {s.Name}");
+        }
+    }
+
+    static void InsertOwner()
+    {
+        // Läsa in namn för ägare
+        Console.Write("Name: ");
+        string name = Chelp.ReadString();
+
+        repo.InsertOwner(name);
+    }
+
+    static void UpdateOwner()
+    {
+        Console.Write("Ange id på den du vill uppdatera: ");
+        int id = Chelp.ReadInt();
+
+        // Uppdatera ägarens namn
+        Console.Write("Name: ");
+        string name = Chelp.ReadString();
+
+        repo.UpdateOwner(id, name);
+    }
+}
