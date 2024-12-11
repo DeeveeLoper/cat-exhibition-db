3f4e10a45ffcc9b5e518f39d7e6fd01cb14100bd
RobCam <roberto.camacho4@gmail.com>
2024-12-11 16:38:36 +0100

    Added CRUD methods for exhibtion and Owner in DatabaseRepository

--- DatabaseRepository.cs
+++ DatabaseRepository.cs
@@ -7,6 +7,8 @@ namespace MyCatExhibition;
 
 class DatabaseRepository
 {
+    //---------------- Owner ----------------------------
+
     public IEnumerable<Owner> GetOwners()
     {
         // Spara i datbasen
@@ -39,5 +41,68 @@ class DatabaseRepository
 
         connection.Execute(query);
     }
+
+    public void DeleteOwner(int id)
+    {
+        // Spara i datbasen
+        string connectionstring = File.ReadAllText("connectionstring.txt");
+        using IDbConnection connection = new SqlConnection(connectionstring);
+
+        string query = $" DELETE FROM Owner WHERE owner_id = {id}";
+
+        connection.Execute(query);
+    }
+
+
+    //---------------- Exhibition ----------------------------
+
+    public IEnumerable<Exhibition> GetExhibitions()
+    {
+        // Spara i datbasen
+        string connectionstring = File.ReadAllText("connectionstring.txt");
+        using IDbConnection connection = new SqlConnection(connectionstring);
+
+        return connection.Query<Exhibition>("SELECT exhibition_id AS ExhibitionId, name AS Name, exhibition_date AS ExhibitionDate FROM exhibition");
+    }
+
+
+    public void InsertExhibition(string name, DateTime exhibtionDate)
+    {
+        // Spara i datbasen
+        string connectionstring = File.ReadAllText("connectionstring.txt");
+        using IDbConnection connection = new SqlConnection(connectionstring);
+
+        string query = $"INSERT INTO exhibition (name, exhibition_date) VALUES ('{name}', '{exhibtionDate}')";
+
+        connection.Execute(query);
+    }
+
+
+    public void UpdateExhibition(int id, string name, DateTime exhibitionDate)
+    {
+        // Spara i datbasen
+        string connectionstring = File.ReadAllText("connectionstring.txt");
+        using IDbConnection connection = new SqlConnection(connectionstring);
+
+        string query = $" UPDATE Exhibition SET name = '{name}', exhibition_date = '{exhibitionDate}' WHERE exhibition_id = {id}";
+
+        connection.Execute(query);
+    }
+
+    public void DeleteExhibition(int id)
+    {
+        // Spara i datbasen
+        string connectionstring = File.ReadAllText("connectionstring.txt");
+        using IDbConnection connection = new SqlConnection(connectionstring);
+
+        string queryTable = $" DELETE FROM cat_to_exhibition WHERE exhibition_id = {id}";
+        connection.Execute(queryTable);
+
+        string queryExhibition = $" DELETE FROM exhibition WHERE exhibition_id = {id}";
+        connection.Execute(queryExhibition);
+    }
+
+
+    //---------------- Cat ----------------------------
 
 }
