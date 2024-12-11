6795bbc7aaa73b850f38085b011272add640d350
RobCam <roberto.camacho4@gmail.com>
2024-12-11 14:45:33 +0100

    Changed the ReadString method

--- CHelp.cs
+++ CHelp.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DapperExercise
+﻿static class Chelp 
 {
-    internal class CHelp
+    public static int ReadInt()
     {
+        int input;
+        while (!int.TryParse(Console.ReadLine(), out input))
+        {
+            Console.WriteLine("Ogiltig inmatning, försök igen");
+        }
+        return input;
+    }
+
+    public static string ReadString() 
+    {
+        string input;
+        while (string.IsNullOrEmpty(input = Console.ReadLine()))
+        {
+            Console.WriteLine("Ogiltigt inmatning, försök igen");
+        }
+        return input;
+    }
+
+    // Användas till exhibition
+    public static DateTime ReadDate()
+    {
+        DateTime input;
+        while (!DateTime.TryParse(Console.ReadLine(), out input))
+        {
+            Console.WriteLine("Ogiltigt datumformat, försöker igen (åååå-mm-dd)");
+        }
+        return input;
     }
 }
